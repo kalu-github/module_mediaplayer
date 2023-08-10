7d4a200f13271e4a5bb41b76469549e56c7f3774
kalu <153437803@qq.com>
2023-08-10 15:24:18 +0800

    update files

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/exo2/VideoExoPlayer2.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/exo2/VideoExoPlayer2.java
@@ -746,8 +746,8 @@ public final class VideoExoPlayer2 extends BasePlayer {
                 OkHttpClient okHttpClient = new OkHttpClient.Builder()
                         .readTimeout(10, TimeUnit.SECONDS)
                         .writeTimeout(10, TimeUnit.SECONDS)
-                        .connectionPool(new ConnectionPool(5, 5, TimeUnit.MINUTES))
-                        .retryOnConnectionFailure(false)
+                        .connectionPool(new ConnectionPool(10, 60, TimeUnit.MINUTES))
+                        .retryOnConnectionFailure(true)
                         .proxySelector(new ProxySelector() { // 禁止抓包
                             @Override
                             public List<Proxy> select(URI uri) {
