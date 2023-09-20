059583fc86b0cb0cd00513a6b71afca2dad8da34
kalu <153437803@qq.com>
2023-09-20 15:43:20 +0800

    update files

---
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/audio/exo2/AudioExoPlayer2Factory.java
@@ -0,0 +1,27 @@
+package lib.kalu.mediaplayer.core.kernel.audio.exo2;
+
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+
+import lib.kalu.mediaplayer.core.kernel.audio.AudioKernelApiEvent;
+import lib.kalu.mediaplayer.core.kernel.audio.AudioKernelFactory;
+import lib.kalu.mediaplayer.core.kernel.video.VideoKernelApiEvent;
+import lib.kalu.mediaplayer.core.kernel.video.VideoKernelFactory;
+import lib.kalu.mediaplayer.core.player.audio.AudioPlayerApi;
+import lib.kalu.mediaplayer.core.player.video.VideoPlayerApi;
+
+@Keep
+public final class AudioExoPlayer2Factory implements AudioKernelFactory<AudioExoPlayer2> {
+
+    private AudioExoPlayer2Factory() {
+    }
+
+    public static AudioExoPlayer2Factory build() {
+        return new AudioExoPlayer2Factory();
+    }
+
+    @Override
+    public AudioExoPlayer2 createKernel(@NonNull AudioPlayerApi playerApi, @NonNull AudioKernelApiEvent event, @NonNull boolean retryBuffering) {
+        return new AudioExoPlayer2(playerApi, event, retryBuffering);
+    }
+}
