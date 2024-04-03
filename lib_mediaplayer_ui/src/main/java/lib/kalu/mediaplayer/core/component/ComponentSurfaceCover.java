f54527df5c0ce3065c2b69103a4f1f6ffdd85177
kalu <153437803@qq.com>
2024-04-03 11:40:06 +0800

    update files

---
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/component/ComponentSurfaceCover.java
@@ -0,0 +1,55 @@
+package lib.kalu.mediaplayer.core.component;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import lib.kalu.mediaplayer.R;
+import lib.kalu.mediaplayer.config.player.PlayerType;
+import lib.kalu.mediaplayer.util.MPLogUtil;
+
+public class ComponentSurfaceCover extends RelativeLayout implements ComponentApi {
+
+    public ComponentSurfaceCover(Context context) {
+        super(context);
+        LayoutInflater.from(getContext()).inflate(R.layout.module_mediaplayer_component_surface_cover, this, true);
+    }
+
+    @Override
+    public void callPlayerEvent(int playState) {
+        switch (playState) {
+            case PlayerType.StateType.STATE_START:
+                MPLogUtil.log("ComponentSurfaceCover => callPlayerEvent => show => playState = " + playState);
+                gone();
+                break;
+            case PlayerType.StateType.STATE_INIT:
+            case PlayerType.StateType.STATE_RELEASE:
+            case PlayerType.StateType.STATE_ERROR:
+            case PlayerType.StateType.STATE_ERROR_IGNORE:
+                MPLogUtil.log("ComponentSurfaceCover => callPlayerEvent => gone => playState = " + playState);
+                show();
+                break;
+        }
+    }
+
+    @Override
+    public final void show() {
+        try {
+            bringToFront();
+            findViewById(R.id.module_mediaplayer_component_surface_cover_bg).setVisibility(View.VISIBLE);
+        } catch (Exception e) {
+            MPLogUtil.log("ComponentSurfaceCover => show => " + e.getMessage());
+        }
+    }
+
+    @Override
+    public final void gone() {
+        try {
+            bringToFront();
+            findViewById(R.id.module_mediaplayer_component_surface_cover_bg).setVisibility(View.GONE);
+        } catch (Exception e) {
+            MPLogUtil.log("ComponentSurfaceCover => gone => " + e.getMessage());
+        }
+    }
+}
