a24cf8414bf744f15d9356a443fb20a704e3f15a
kalu <153437803@qq.com>
2024-05-09 18:02:15 +0800

    update files

--- app/src/main/java/com/kalu/mediaplayer/App.java
+++ app/src/main/java/com/kalu/mediaplayer/App.java
@@ -1,8 +1,17 @@
 package com.kalu.mediaplayer;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
 
 public class App extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
