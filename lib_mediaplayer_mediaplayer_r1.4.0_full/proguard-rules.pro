68d2a5aa20fc41f9ff0a24b97865977240eb8f21
kalu <153437803@qq.com>
2024-07-15 11:20:22 +0800

    update files

---
+++ lib_mediaplayer_mediaplayer_r1.4.0_full/proguard-rules.pro
@@ -0,0 +1,20 @@
+# exoplayet-rtmp
+-dontwarn lib.kalu.mediax.rtmp.**
+-keep class lib.kalu.mediax.rtmp.** { *; }
+-keep interface lib.kalu.mediax.rtmp.** { *; }
+# exoplayet-ffmpeg
+-dontwarn lib.kalu.mediax.ffmpeg.**
+-keep class lib.kalu.mediax.ffmpeg.** { *; }
+-keep interface lib.kalu.mediax.ffmpeg.** { *; }
+# exoplayet-log
+-dontwarn lib.kalu.mediax.util.**
+-keep class lib.kalu.mediax.util.** { *; }
+-keep interface lib.kalu.mediax.util.** { *; }
+## exoplayet-video
+#-dontwarn com.google.android.exoplayer2.video.**
+#-keep class com.google.android.exoplayer2.video.** { *; }
+#-keep interface com.google.android.exoplayer2.video.** { *; }
+## exoplayet-audio
+#-dontwarn com.google.android.exoplayer2.audio.**
+#-keep class com.google.android.exoplayer2.audio.** { *; }
+#-keep interface com.google.android.exoplayer2.audio.** { *; }
---
+++ lib_mediaplayer_mediaplayer_r1.4.0_full/src/main/java/lib/kalu/mediax/ffmpeg/BaseOnlyMediaCodecVideoRenderersFactory.java
@@ -0,0 +1,40 @@
+package lib.kalu.mediax.ffmpeg;
+
+import android.content.Context;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.exoplayer.DefaultRenderersFactory;
+import androidx.media3.exoplayer.Renderer;
+import androidx.media3.exoplayer.audio.AudioRendererEventListener;
+import androidx.media3.exoplayer.audio.AudioSink;
+import androidx.media3.exoplayer.audio.MediaCodecAudioRenderer;
+import androidx.media3.exoplayer.mediacodec.MediaCodecSelector;
+import androidx.media3.exoplayer.video.MediaCodecVideoRenderer;
+import androidx.media3.exoplayer.video.VideoRendererEventListener;
+
+import java.util.ArrayList;
+
+import lib.kalu.mediax.util.ExoLogUtil;
+
+public class BaseOnlyMediaCodecVideoRenderersFactory extends BaseRenderersFactory {
+
+    public BaseOnlyMediaCodecVideoRenderersFactory(Context context) {
+        super(context);
+        ExoLogUtil.log("BaseRenderersFactory => BaseOnlyMediaCodecVideoRenderersFactory =>");
+    }
+
+    @Override
+    protected void addMediaCodecAudioRenderer(@NonNull Context context, @ExtensionRendererMode int extensionRendererMode, @NonNull MediaCodecSelector mediaCodecSelector, @NonNull boolean enableDecoderFallback, @NonNull AudioSink audioSink, @NonNull Handler eventHandler, @NonNull AudioRendererEventListener eventListener, @NonNull ArrayList<Renderer> out) {
+    }
+
+    @Override
+    protected void addFFmpegAudioRenderers(@NonNull ArrayList<Renderer> out) {
+    }
+
+    @Override
+    protected void addFFmpegVideoRenderers(long allowedJoiningTimeMs, @Nullable Handler eventHandler, @Nullable VideoRendererEventListener eventListener, int maxDroppedFramesToNotify, @NonNull ArrayList<Renderer> out) {
+    }
+}
