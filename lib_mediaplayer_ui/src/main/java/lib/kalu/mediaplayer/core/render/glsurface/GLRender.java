ac95ee81918fe494e54e42ec1a01aa02eec4daf2
kalu <153437803@qq.com>
2024-07-18 13:35:22 +0800

    update files

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/render/glsurface/GLRender.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/render/glsurface/GLRender.java
@@ -9,36 +9,49 @@ import java.util.List;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import lib.kalu.mediaplayer.util.LogUtil;
+
 final class GLRender implements GLSurfaceView.Renderer {
     private final List<GLDrawer> drawers = new ArrayList<GLDrawer>();
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0f, 0f, 0f, 0f);
-        //开启混合，即半透明
-        GLES20.glEnable(GLES20.GL_BLEND);
-        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
-        int[] textureIds = GLTool.getInstance().createTextureIds(drawers.size());
-        for (int i = 0; i < textureIds.length; i++) {
-            drawers.get(i).setTextureID(textureIds[i]);
+        try {
+            GLES20.glClearColor(0f, 0f, 0f, 0f);
+            //开启混合，即半透明
+            GLES20.glEnable(GLES20.GL_BLEND);
+            GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+            int[] textureIds = GLTool.getInstance().createTextureIds(drawers.size());
+            for (int i = 0; i < textureIds.length; i++) {
+                drawers.get(i).setTextureID(textureIds[i]);
+            }
+        } catch (Exception e) {
+            LogUtil.log("GLRender => onSurfaceCreated => Exception " + e.getMessage());
         }
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
-        for (GLDrawer drawer : drawers) {
-            drawer.setWorldSize(width, height);
+        try {
+            GLES20.glViewport(0, 0, width, height);
+            for (GLDrawer drawer : drawers) {
+                drawer.setWorldSize(width, height);
+            }
+        } catch (Exception e) {
+            LogUtil.log("GLRender => onSurfaceChanged => Exception " + e.getMessage());
         }
-
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         //清除颜色缓冲和深度缓冲
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-        for (int i = 0; i < drawers.size(); i++) {
-            drawers.get(i).draw();
+        try {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+            for (int i = 0; i < drawers.size(); i++) {
+                drawers.get(i).draw();
+            }
+        } catch (Exception e) {
+            LogUtil.log("GLRender => onDrawFrame => Exception " + e.getMessage());
         }
     }
 
@@ -48,6 +61,10 @@ final class GLRender implements GLSurfaceView.Renderer {
      * @param drawer
      */
     public void addDrawer(GLDrawer drawer) {
-        drawers.add(drawer);
+        try {
+            drawers.add(drawer);
+        } catch (Exception e) {
+            LogUtil.log("GLRender => addDrawer => Exception " + e.getMessage());
+        }
     }
 }
